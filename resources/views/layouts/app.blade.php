a10637e84d2b01723b5c4647b724c6fb6d38ec05
jiro.kirameki <ec2-user@ip-172-31-35-132.us-east-2.compute.internal>
2018-06-01 01:41:07 +0000

    teraada

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,10 +15,15 @@
 
     <body>
         @include('commons.navbar')
+
         <div class = "container">
-            @include('commons.error_tasks')
-    
-            @yield('content')
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+                    @include('commons.error_tasks')
+                
+                    @yield('content')
+                </div>
+            </div>
         </div>
     </body>
 </html>
