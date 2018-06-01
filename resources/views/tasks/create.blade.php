4f90a03f4665022e904173b32bebfadbf9238a51
jiro.kirameki <ec2-user@ip-172-31-35-132.us-east-2.compute.internal>
2018-06-01 07:25:41 +0000

    heroku

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -11,7 +11,7 @@
             {!! Form::text('status', null, ['class' => 'form-control']) !!}
         </div>
         <div class="form-group">
-            {!! Form::label('content', 'メッセージ:') !!}
+            {!! Form::label('content', 'タスク:') !!}
             {!! Form::text('content', null, ['class' => 'form-control']) !!}
         </div>
                 
