76d6f9c88398cc20af4a8a181691d11cd5453c03
jiro.kirameki <ec2-user@ip-172-31-35-132.us-east-2.compute.internal>
2018-06-06 08:29:37 +0000

    key

--- database/migrations/2018_06_05_141325_add_id_to_tasks_table.php
+++ database/migrations/2018_06_05_141325_add_id_to_tasks_table.php
@@ -14,7 +14,9 @@ class AddIdToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->integer('user_id');
+            $table->integer('user_id')->unsigned()->index();
+            
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
